1966142bf48461e169584f9af176864f563a0cc4
zhuping <hzzhuping@corp.netease.com>
2017-08-20 20:55:06 +0800

    update

--- js/dotting.js
+++ js/dotting.js
@@ -1,5 +1,8 @@
 var isTestEnv = true;
+var isKaolaEnv = false;
 var allScripts = document.getElementsByTagName('script');
+var allLinks = document.getElementsByTagName('link');
+
 
 // 根据页面包含统计脚本来判断是否是测试环境
 for (var i = 0; i < allScripts.length; i++) {
@@ -9,8 +12,16 @@ for (var i = 0; i < allScripts.length; i++) {
   }
 }
 
-if (isTestEnv) {
+// 判断是否是考拉相关的环境
+for (var j= 0; j < allLinks.length; j++) {
+  if (allLinks[j].href.indexOf('kaola') > -1) {
+    isKaolaEnv = true;
+    break;
+  }
+}
+
+if (isTestEnv && isKaolaEnv) {
   var script = document.createElement('script');
   script.src = '//haitao.nos.netease.com/2f5267e0-b803-4478-ba29-fe13120cac59.js';
-  allScripts[0].parentNode.insertBefore(script, allScripts[0]);
+  allScripts[0] && allScripts[0].parentNode.insertBefore(script, allScripts[0]);
 }
